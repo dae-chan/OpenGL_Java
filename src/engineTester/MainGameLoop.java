f6a01651eaa8831fc8691045350b4c9edc71ca6f
jdc <jang9129@gmail.com>
2018-09-19 00:31:04 +0900

    OpenGL 3D Game Tutorial 8: Model, View & Projection Matrices - 2
     - change camera position with using view matrix.
     - https://youtu.be/50Y9u7K0PZo

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -4,6 +4,7 @@ package engineTester;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.util.vector.Vector3f;
 
+import entities.Camera;
 import entities.Entity;
 import models.RawModel;
 import models.TexturedModel;
@@ -21,27 +22,82 @@ public class MainGameLoop {
         DisplayManager.createDisplay();
 
         Loader loader = new Loader();
-        Renderer renderer = new Renderer();
         // Bind 0 to 'position' and 1 to 'uv'
         StaticShader shader = new StaticShader();
+        Renderer renderer = new Renderer(shader);
 
         float[] vertices = {
-                -0.5f, 0.5f, 0f, // V0
-                -0.5f, -0.5f, 0f, // V1
-                0.5f, -0.5f, 0f, // V2
-                0.5f, 0.5f, 0f // V3
-        };
+                -0.5f, 0.5f, -0.5f,
+                -0.5f, -0.5f, -0.5f,
+                0.5f, -0.5f, -0.5f,
+                0.5f, 0.5f, -0.5f,
 
-        int[] indices = {
-                0, 1, 3, // Top left triangle (V0, V1, V3)
-                3, 1, 2 // Bottom right triangle (V3, V1, V2)
+                -0.5f, 0.5f, 0.5f,
+                -0.5f, -0.5f, 0.5f,
+                0.5f, -0.5f, 0.5f,
+                0.5f, 0.5f, 0.5f,
+
+                0.5f, 0.5f, -0.5f,
+                0.5f, -0.5f, -0.5f,
+                0.5f, -0.5f, 0.5f,
+                0.5f, 0.5f, 0.5f,
+
+                -0.5f, 0.5f, -0.5f,
+                -0.5f, -0.5f, -0.5f,
+                -0.5f, -0.5f, 0.5f,
+                -0.5f, 0.5f, 0.5f,
+
+                -0.5f, 0.5f, 0.5f,
+                -0.5f, 0.5f, -0.5f,
+                0.5f, 0.5f, -0.5f,
+                0.5f, 0.5f, 0.5f,
+
+                -0.5f, -0.5f, 0.5f,
+                -0.5f, -0.5f, -0.5f,
+                0.5f, -0.5f, -0.5f,
+                0.5f, -0.5f, 0.5f
         };
 
         float[] textureCoords = {
-                0, 0, // V0
-                0, 1, // V1
-                1, 1, // V2
-                1, 0 // V3
+                0, 0,
+                0, 1,
+                1, 1,
+                1, 0,
+                0, 0,
+                0, 1,
+                1, 1,
+                1, 0,
+                0, 0,
+                0, 1,
+                1, 1,
+                1, 0,
+                0, 0,
+                0, 1,
+                1, 1,
+                1, 0,
+                0, 0,
+                0, 1,
+                1, 1,
+                1, 0,
+                0, 0,
+                0, 1,
+                1, 1,
+                1, 0
+        };
+
+        int[] indices = {
+                0, 1, 3,
+                3, 1, 2,
+                4, 5, 7,
+                7, 5, 6,
+                8, 9, 11,
+                11, 9, 10,
+                12, 13, 15,
+                15, 13, 14,
+                16, 17, 19,
+                19, 17, 18,
+                20, 21, 23,
+                23, 21, 22
         };
 
         // Load data to VAO
@@ -50,15 +106,18 @@ public class MainGameLoop {
         ModelTexture texture = new ModelTexture(loader.loadTexture("uvtemplate"));
         TexturedModel texturedModel = new TexturedModel(model, texture);
 
-        Entity entity = new Entity(texturedModel, new Vector3f(-1, 0, 0), 0, 0, 0, 1);
+        Entity entity = new Entity(texturedModel, new Vector3f(0, 0, -5), 0, 0, 0, 1);
+
+        Camera camera = new Camera();
 
         while (!Display.isCloseRequested()) {
-            entity.increasePosition(0.002f, 0, 0);
-            entity.increaseRotation(0, 0.01f, 0);
+            entity.increaseRotation(0.01f, 0.01f, 0);
+            camera.move();
             renderer.prepare();
             // game logic
             // render
             shader.start();
+            shader.loadViewMatrix(camera);
             renderer.render(entity, shader);
             shader.stop();
             DisplayManager.updateDisplay();
--- src/shaders/StaticShader.java
+++ src/shaders/StaticShader.java
@@ -3,12 +3,17 @@ package shaders;
 
 import org.lwjgl.util.vector.Matrix4f;
 
+import entities.Camera;
+import toolbox.Maths;
+
 public class StaticShader extends ShaderProgram {
 
     private static final String VERTEX_FILE = "src/shaders/vertexShader.txt";
     private static final String FRAGMENT_FILE = "src/shaders/fragmentShader.txt";
 
     private int location_transformationMatrix;
+    private int location_projectionMatrix;
+    private int location_viewMatrix;
 
     public StaticShader() {
         super(VERTEX_FILE, FRAGMENT_FILE);
@@ -26,10 +31,21 @@ public class StaticShader extends ShaderProgram {
     protected void getAllUniformLocations() {
         // TODO Auto-generated method stub
         location_transformationMatrix = super.getUniformLocation("transformationMatrix");
+        location_projectionMatrix = super.getUniformLocation("projectionMatrix");
+        location_viewMatrix = super.getUniformLocation("viewMatrix");
     }
 
     public void loadTransformationmatrix(Matrix4f matrix) {
         super.loadMatrix(location_transformationMatrix, matrix);
+    }
+
+    public void loadProjectionMatrix(Matrix4f matrix) {
+        super.loadMatrix(location_projectionMatrix, matrix);
+    }
+
+    public void loadViewMatrix(Camera camera) {
+        Matrix4f viewMatrix = Maths.createViewMatrix(camera);
+        super.loadMatrix(location_viewMatrix, viewMatrix);
     }
 
 }
